2ab85ef9bda77b336ddc1eeaf7b24015ff6dca8c
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-01-09 17:51:21 -0600

    Added and initialized firebase in project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwCv3GbuWAhr4Oix3Lvdpj7Imxb1a6DQs",
+    authDomain: "daily-moments-1a056.firebaseapp.com",
+    projectId: "daily-moments-1a056",
+    storageBucket: "daily-moments-1a056.appspot.com",
+    messagingSenderId: "208906914199",
+    appId: "1:208906914199:web:691d9df0d639364d5d5cd2"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
