a912f39708702fb6bd91099c6be4408ccdb6d0cd
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-01-09 18:47:54 -0600

    Added loading component to login while determining if user is authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { IonApp } from '@ionic/react';
+import { IonApp, IonLoading } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import {  Redirect, Route, Switch } from 'react-router-dom';
 import { AuthContext } from './Auth';
@@ -10,17 +10,20 @@ import { auth } from './firebase'
 
 
 const App: React.FC = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [authState, setAuthState] = useState({loading: true, loggedIn: false});
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setLoggedIn(Boolean(user))
+      setAuthState(({loading: false, loggedIn: Boolean(user)}))
     })    
   }, [])
 
-  console.log(`rending App with loggedIn=${loggedIn}`);
+  console.log(`rending App with loggedIn=`, authState);
+  if(authState.loading) {
+    return <IonLoading isOpen />
+  }
   return (
     <IonApp>
-      <AuthContext.Provider value={{loggedIn}}>
+      <AuthContext.Provider value={{loggedIn: authState.loggedIn}}>
         <IonReactRouter>
           <Switch>
             <Redirect exact path='/' to="/login"/>
