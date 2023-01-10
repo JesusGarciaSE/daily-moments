5f52ee9ef6072ce61864db9ec6b1a6fe18903295
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-01-09 18:50:16 -0600

    Implemented logout under settings

--- src/Pages/SettingsPage.tsx
+++ src/Pages/SettingsPage.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { auth } from '../firebase';
 
 const SettingsPage: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const SettingsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        This is the settings page.
+        <IonButton expand='block' color="medium" onClick={() => auth.signOut()}>Logout</IonButton>
       </IonContent>
     </IonPage>
   );
