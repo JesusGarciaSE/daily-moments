ecb2458f57c0dc9adc2c4d71c3ef1f3c4572fd09
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-01-27 16:48:51 -0600

    Added functionality to add a new entry to AddNewEntry

--- src/Pages/AddEntryPage.tsx
+++ src/Pages/AddEntryPage.tsx
@@ -1,6 +1,24 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
+import { useAuth } from '../Auth';
+import { firestore } from '../firebase';
+import { addDoc, collection } from 'firebase/firestore';
+import { useHistory } from 'react-router';
 
 const AddEntryPage: React.FC = () => {
+  const { userId } = useAuth();
+  const history = useHistory();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = async() => {
+    const entriesRef = collection(firestore, `users/${userId}/entries`);
+    const entryData = { title, description };
+    const entryId = await addDoc(entriesRef, entryData);
+    console.log('saved:', entryId.id);
+    history.goBack()
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -12,7 +30,17 @@ const AddEntryPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        Todo
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Title</IonLabel>
+            <IonInput value={title} onIonChange={(event) => setTitle(event.detail.value)} />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Description</IonLabel>
+            <IonTextarea value={description} onIonChange={(event) => setDescription(event.detail.value)}/>
+          </IonItem>
+        </IonList>
+        <IonButton expand="block" onClick={handleSubmit}>Save</IonButton>
       </IonContent>
     </IonPage>
   );
