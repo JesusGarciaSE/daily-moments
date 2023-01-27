06b0c4dd4f86744fe39192d9fabffab38d8c2310
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2023-01-27 16:05:02 -0600

    Added toEntry function to models.ts

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,11 @@
+import { DocumentSnapshot } from "firebase/firestore";
+
 export interface Entry {
     id: string;
     title: string;
     description: string;
+}
+
+export function toEntry(doc: DocumentSnapshot): Entry {
+    return { id: doc.id, ...doc.data() } as Entry;
 }
